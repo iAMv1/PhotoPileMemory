195e341380d6c1383f5f9c1d66622718cf24d992
Pratham <iam1nahata@gmail.com>
2026-01-12 00:54:04 +0530

    some bugs fixed

--- verify-api.js
+++
@@ -1,5 +0,0 @@
-
-fetch('http://localhost:5000/api/time-capsule-messages/current')
-    .then(res => res.json())
-    .then(data => console.log('API Response:', JSON.stringify(data, null, 2)))
-    .catch(err => console.error('Verification Error:', err.message));
